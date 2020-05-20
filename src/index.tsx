63645f8cd188c27b8c30137282bbb5bd58abaa92
Noble Obioma <nobioma1@gmail.com>
2020-05-20 20:05:25 +0100

    Move app component to single file

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <h1>App</h1>;
-};
+import { App } from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
