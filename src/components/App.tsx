ee0bf6d86bc147ad846bfbe60eac72009583bdce
Noble Obioma <nobioma1@gmail.com>
2020-05-21 10:25:05 +0100

    Add Loader, render loader if loading in state is true

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { Footer } from './Footer';
 import { Header } from './Header';
@@ -7,20 +8,24 @@ import { Post } from './Post';
 import { Posts } from './Posts';
 import { GlobalStyle } from './shared/GlobalStyle';
 import { Container } from './shared/Container';
+import { Loader } from './shared/Loader';
 
 export const App = () => {
+  const { loading } = useSelector(({ postsReducer }: any) => postsReducer);
+
   return (
     <Router>
       <GlobalStyle />
       <div>
         <Header />
         <Container>
-        <Switch>
-          <Route path="/" component={Posts} exact />
-          <Route path="/:slug" component={Post} />
-        </Switch>
+          <Switch>
+            <Route path="/" component={Posts} exact />
+            <Route path="/:slug" component={Post} />
+          </Switch>
         </Container>
         <Footer />
+        {loading && <Loader />}
       </div>
     </Router>
   );
