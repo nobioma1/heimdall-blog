ab5dac932c9ad56e83dc04a4c8e03e89188797d7
Noble Obioma <nobioma1@gmail.com>
2020-05-21 10:24:18 +0100

    Add Global Style, Wrap Routes in Container Component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,16 +5,21 @@ import { Footer } from './Footer';
 import { Header } from './Header';
 import { Post } from './Post';
 import { Posts } from './Posts';
+import { GlobalStyle } from './shared/GlobalStyle';
+import { Container } from './shared/Container';
 
 export const App = () => {
   return (
     <Router>
+      <GlobalStyle />
       <div>
         <Header />
+        <Container>
         <Switch>
           <Route path="/" component={Posts} exact />
           <Route path="/:slug" component={Post} />
         </Switch>
+        </Container>
         <Footer />
       </div>
     </Router>
