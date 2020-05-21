60a8bc7720ee15df000c6b04cd1f9d4a2f13521a
Noble Obioma <nobioma1@gmail.com>
2020-05-21 10:22:56 +0100

    Create action creator to fetchPost (single post by slug) - dispatch action FETCH_START on start - dispatch action FETCH_POSTS on success  with payload - dispatch action FETCH_ERROR on error

--- src/actions/posts.ts
+++ src/actions/posts.ts
@@ -21,3 +21,23 @@ export const fetchPosts = ({
   }
 };
 
+export const fetchPost = ({
+  slug,
+}: {
+  slug: string;
+}): ThunkAction<void, AppState, null, Action<string>> => async (dispatch) => {
+  dispatch({ type: ActionTypes.FETCH_START });
+  try {
+    const response = await axios.get(
+      `https://epower.ng/wp-json/wp/v2/posts?slug=${slug}`
+    );
+    if (response.data[0]) {
+      dispatch({ type: ActionTypes.FETCH_POST, payload: response.data[0] });
+    }
+  } catch (error) {
+    dispatch({
+      type: ActionTypes.FETCH_ERROR,
+      payload: 'Sorry, error displaying post at the moment',
+    });
+  }
+};
