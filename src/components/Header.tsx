c7f02d0943d2c211bbde1076f8dbe93e3f0dc993
Noble Obioma <nobioma1@gmail.com>
2020-05-21 10:26:44 +0100

    Conditionally render default hearder text or post titile and post if Post

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,41 @@
 import React from 'react';
+import moment from 'moment';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+import { AppState } from '../store';
+
+const HeaderContainer = styled.div`
+  align-items: center;
+  background-color: rgb(24, 136, 165);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 20vh;
+  width: 100%;
+  padding: 0 40px;
+
+  h1 {
+    color: #fff;
+    font-size: 4rem;
+    text-align: center;
+    margin: 5px 0;
+  }
+
+  p {
+    color: #fff;
+    font-size: 2.5rem;
+    text-transform: uppercase;
+  }
+`;
 
 export const Header = () => {
-  return <h1>Header</h1>;
+  const { post } = useSelector(({ postsReducer }: AppState) => postsReducer);
+
+  return (
+    <HeaderContainer>
+      <h1>{post ? post.title.rendered : 'Epower Blog'}</h1>
+      {post && <p>PUBLISHED ON: {moment(post.date).format('MMMM D, YYYY')}</p>}
+    </HeaderContainer>
+  );
 };
