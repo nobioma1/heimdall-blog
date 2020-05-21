fec83b02ff17d4a0f9c477da40533cf2d706a814
Noble Obioma <nobioma1@gmail.com>
2020-05-21 10:30:11 +0100

    Create Global style

---
+++ src/components/shared/GlobalStyle.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  *,html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+    font-weight: 500;
+    margin: 0
+  }
+`;
