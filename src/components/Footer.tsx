4cd776319f1317fba596f7bc971c083f6e4eea35
Noble Obioma <nobioma1@gmail.com>
2020-05-21 10:26:09 +0100

    Style footer Component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.div`
+  align-items: center;
+  background-color: rgb(24, 136, 165);
+  display: flex;
+  justify-content: center;
+  height: 10vh;
+  width: 100%;
+
+  p {
+    color: #fff;
+    font-size: 2.5rem;
+    text-align: center;
+  }
+`;
 
 export const Footer = () => {
   return (
-    <div>
+    <FooterContainer>
       <p>Copyright 2019</p>
-    </div>
+    </FooterContainer>
   );
 };
